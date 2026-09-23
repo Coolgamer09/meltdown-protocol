fe58000037fa27dee2009a079d4b5b7415e3a8d0
Coolgamer09 <146441932+Coolgamer09@users.noreply.github.com>
2026-09-23 10:53:08 -0400

    Add vent object with cooling functionality

--- game/vents.js
+++ game/vents.js
@@ -1,1 +1,15 @@
+const vent = {
+  id: "vent",
+  name: "vent",
+  type: "vent",
+  heat: 0,
+  coolingPower: 5,
 
+  useOneTick() {
+    this.heat = this.heat - this.coolingPower;
+
+    if (this.heat < 0) {
+      this.heat = 0;
+    }
+  }
+};
