e3bd29c5e4e6bfc2dd58a1f47375c384591a937e
Coolgamer09 <146441932+Coolgamer09@users.noreply.github.com>
2026-09-23 09:35:08 -0400

    Add uranium cell with heat and power production

--- game/cells.js
+++ game/cells.js
@@ -1,1 +1,28 @@
+const uranium = {
+  name: "uranium",
+  type: "cell",
 
+  heat: 0,
+  power: 0,
+
+  active: true,
+  timeLeft: 15,
+
+  produceHeatAndPower() {
+    if (this.active === false) {
+      return;
+    }
+
+    this.heat += 1;
+    this.power += 1;
+  },
+
+  useTime() {
+    this.timeLeft -= 1;
+
+    if (this.timeLeft <= 0) {
+      this.active = false;
+      this.timeLeft = 0;
+    }
+  }
+};
