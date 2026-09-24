5c6a37dacf91370caafcb9d967729f1f42aa1480
Simply-Mystic <146441932+Coolgamer09@users.noreply.github.com>
2026-09-24 09:19:00 -0400

    Run thermal inlet and outlet passively during each game tick

--- game/script.js
+++ game/script.js
@@ -6,6 +6,14 @@ function gameTick() {
   if (!reactor.exploded) {
     reactor.runTick();
 
+    if (typeof inlet !== "undefined") {
+      inlet.injectIntoReactor(reactor);
+    }
+
+    if (typeof outlet !== "undefined") {
+      outlet.extractFromReactor(reactor);
+    }
+
     if (typeof capacitorBank !== "undefined") {
       const soldPower = reactor.power * capacitorBank.currentSellRate();
       reactor.power -= soldPower;
@@ -17,6 +25,8 @@ function gameTick() {
     maxHeat: reactor.maxHeat,
     reactorPower: reactor.power,
     maxStoredPower: reactor.maxStoredPower,
+    inletHeat: typeof inlet === "undefined" ? 0 : inlet.heat,
+    outletHeat: typeof outlet === "undefined" ? 0 : outlet.heat,
     heatPlates: typeof heatPlateBank === "undefined" ? 0 : heatPlateBank.items.length
   });
 }
